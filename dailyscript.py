9e39ba862da56e60a6a994e273b2666d96829b10
Aveek Saha <aveek.s98@gmail.com>
2020-06-17 01:00:00 +0300

    Handle different filetypes

--- dailyscript.py
+++ dailyscript.py
@@ -2,11 +2,19 @@ from bs4 import BeautifulSoup
 import urllib
 import os
 from tqdm import tqdm
+import string
 
 ALL_URL_1 = "https://www.dailyscript.com/movie.html"
 ALL_URL_2 = "https://www.dailyscript.com/movie_n-z.html"
 BASE_URL = "https://www.dailyscript.com/"
 DIR = os.path.join("scripts", "dailyscript")
+
+
+def format_filename(s):
+    valid_chars = "-_,() %s%s" % (string.ascii_letters, string.digits)
+    filename = ''.join(c for c in s if c in valid_chars)
+    filename = filename.replace(' ', '_')  # I don't like spaces in filenames.
+    return filename
 
 
 def get_soup(url):
@@ -32,23 +40,36 @@ for movie in tqdm(movielist):
     if len(script_url) < 2:
         continue
     script_url = movie.find('a').get('href')
-    print(script_url)
+    # print(script_url)
 
     text = ""
 
     if script_url.endswith('.pdf'):
         continue
 
-    elif script_url.endswith('.html') or script_url.endswith('.htm'):
+    elif script_url.endswith('.html'):
+        script_soup = get_soup(BASE_URL + urllib.parse.quote(script_url))
+        doc = script_soup.pre
+        if doc:
+            text = script_soup.pre.get_text()
+        else:
+            text = script_soup.get_text()
+        name = script_url.split("/")[-1].strip('.html')
+    
+    elif script_url.endswith('.htm'):
         script_soup = get_soup(BASE_URL + urllib.parse.quote(script_url))
         text = script_soup.pre.get_text()
+        name = script_url.split("/")[-1].strip('.htm')
     
     elif script_url.endswith('.txt'):
         script_soup = get_soup(BASE_URL + urllib.parse.quote(script_url))
         text = script_soup.get_text()
+        name = script_url.split("/")[-1].strip('.txt')
 
     if text == "":
         continue
 
+    name = format_filename(name)
+    
     with open(os.path.join(DIR, name + '.txt'), 'w', errors="ignore") as out:
         out.write(text)
