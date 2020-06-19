22bdc0edc74ac58c0454e42baa48586ca420023c
Aveek Saha <aveek.s98@gmail.com>
2020-06-19 01:00:00 +0100

    Add utils for common functions

---
+++ utilities.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import urllib
+import string
+
+def format_filename(s):
+    valid_chars = "-() %s%s%s" % (string.ascii_letters, string.digits, "%")
+    filename = ''.join(c for c in s if c in valid_chars)
+    filename = filename.replace('%20', ' ')
+    filename = filename.replace('%27', ' ')
+    filename = filename.replace(' ', '-')
+    return filename
+
+
+def get_soup(url):
+    page = urllib.request.Request(url)
+    result = urllib.request.urlopen(page)
+    resulttext = result.read()
+
+    soup = BeautifulSoup(resulttext, 'html.parser')
+    return soup
