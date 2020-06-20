d7f2cdc5165e11564642bfd6c8c2c17e09d77dac
Aveek Saha <aveek.s98@gmail.com>
2020-06-20 01:00:00 +0100

    Add function to read text from PDF

--- utilities.py
+++ utilities.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import urllib
 import string
+import os
+import textract
 
 def format_filename(s):
     valid_chars = "-() %s%s%s" % (string.ascii_letters, string.digits, "%")
@@ -18,3 +20,12 @@ def get_soup(url):
 
     soup = BeautifulSoup(resulttext, 'html.parser')
     return soup
+
+def get_pdf_text(url):
+    result = urllib.request.urlopen(url)
+    f = open(os.path.join("scripts", "document.pdf"), 'wb')
+    f.write(response.read())
+    f.close()
+    text = textract.process(os.path.join(
+        "scripts", "document.pdf"), encoding='utf-8').decode('utf-8')
+    return text
