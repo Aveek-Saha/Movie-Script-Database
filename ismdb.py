63e977e1a84150547e3cdf8eaa68dc6f981314c2
Aveek Saha <aveek.s98@gmail.com>
2020-06-21 01:00:00 +0100

    Get PDFs from ismdb

--- ismdb.py
+++ ismdb.py
@@ -6,7 +6,7 @@ import time
 import string
 
 from tqdm import tqdm
-from utilities import format_filename, get_soup
+from utilities import format_filename, get_soup, get_pdf_text
 
 ALL_URL = "https://www.imsdb.com/all%20scripts"
 BASE_URL = "https://www.imsdb.com"
@@ -17,23 +17,26 @@ if not os.path.exists(DIR):
 
 
 def get_script_from_url(script_url):
-    if not script_url.endswith('.html'):
-        return ""
-
-    script_soup = get_soup(BASE_URL + urllib.parse.quote(script_url))
-    if len(script_soup.find_all('td', class_="scrtext")) < 1:
-        return ""
-    script_text = script_soup.find_all('td', class_="scrtext")[0].pre
-
     text = ""
-    if script_text:
-        script_text = script_soup.find_all('td', class_="scrtext")[0].pre.pre
-        if script_text:
-            text = script_text.get_text()
 
-        else:
-            script_text = script_soup.find_all('td', class_="scrtext")[0].pre
-            text = script_text.get_text()
+    if script_url.endswith('.pdf'):
+        text = get_pdf_text(script_url)
+        return text
+
+    if script_url.endswith('.html'):
+        script_soup = get_soup(BASE_URL + urllib.parse.quote(script_url))
+        if len(script_soup.find_all('td', class_="scrtext")) < 1:
+            return ""
+        script_text = script_soup.find_all('td', class_="scrtext")[0].pre
+
+        if script_text:
+            script_text = script_soup.find_all('td', class_="scrtext")[0].pre.pre
+            if script_text:
+                text = script_text.get_text()
+
+            else:
+                script_text = script_soup.find_all('td', class_="scrtext")[0].pre
+                text = script_text.get_text()
 
     return text
 
