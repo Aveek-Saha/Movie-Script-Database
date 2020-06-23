bb85fc7e1d8276035817af1ed6da04c8112da189
Aveek Saha <aveek.s98@gmail.com>
2020-06-23 01:00:00 +0100

    Clean up the clean up

--- clean_files.py
+++ clean_files.py
@@ -11,6 +11,7 @@ DIR_WEEKLY = join("scripts", "weeklyscript")
 DIR_SCREEN = join("scripts", "screenplays")
 DIR_AWESOME = join("scripts", "awesomefilm")
 DIR_SAVANT = join("scripts", "scriptsavant")
+DIR_SFY = join("scripts", "sfy")
 
 DIR_FILTER = join("scripts", "filtered")
 DIR_FINAL = join("scripts", "final")
@@ -27,6 +28,18 @@ awesome = [join(DIR_AWESOME, f) for f in listdir(DIR_AWESOME) if isfile(
     join(DIR_AWESOME, f))and getsize(join(DIR_AWESOME, f)) > 3000]
 savant = [join(DIR_SAVANT, f) for f in listdir(DIR_SAVANT) if isfile(
     join(DIR_SAVANT, f))and getsize(join(DIR_SAVANT, f)) > 3000]
+sfy = [join(DIR_SFY, f) for f in listdir(DIR_SFY) if isfile(
+    join(DIR_SFY, f))and getsize(join(DIR_SFY, f)) > 3000]
+
+sources = {
+    'ismdb': ismdb,
+    'daily': daily,
+    'weekly': weekly,
+    'screen': screen,
+    'awesome': awesome,
+    'savant': savant,
+    'sfy': sfy
+}
 
 
 def remove_duplicates(arr, comb):
@@ -56,138 +69,110 @@ def remove_duplicates(arr, comb):
 
     return arr
 
+for key in sources:
+    arr = sources[key]
+    print("Remove duplicates from", key, len(arr))
+    comb = list(itertools.combinations(arr, 2))
+    arr = remove_duplicates(arr, comb)
+    print("Non duplicates", len(arr))
+    print()
 
-print("Remove duplicates from ismdb ", len(ismdb))
-comb_ismdb = list(itertools.combinations(ismdb, 2))
-ismdb = remove_duplicates(ismdb, comb_ismdb)
-print("Non duplicates", len(ismdb))
-print()
 
-print("Remove duplicates from dailyscript ", len(daily))
-comb_daily = list(itertools.combinations(daily, 2))
-daily = remove_duplicates(daily, comb_daily)
-print("Non duplicates", len(daily))
-print()
-
-print("Remove duplicates from weeklyscript ", len(weekly))
-comb_weekly = list(itertools.combinations(weekly, 2))
-weekly = remove_duplicates(weekly, comb_weekly)
-print("Non duplicates", len(weekly))
-print()
-
-print("Remove duplicates from screenplays-online ", len(screen))
-comb_screen = list(itertools.combinations(screen, 2))
-screen = remove_duplicates(screen, comb_screen)
-print("Non duplicates", len(screen))
-print()
-
-print("Remove duplicates from awesomefilm ", len(awesome))
-comb_awesome = list(itertools.combinations(awesome, 2))
-awesome = remove_duplicates(awesome, comb_awesome)
-print("Non duplicates", len(awesome))
-print()
-
-print("Remove duplicates from scriptsavant ", len(savant))
-comb_savant = list(itertools.combinations(savant, 2))
-savant = remove_duplicates(savant, comb_savant)
-print("Non duplicates", len(savant))
-print()
-
-print("Remove duplicates between sources")
-all_sources = ismdb + daily
-print(len(all_sources))
-comb_all = list(itertools.combinations(all_sources, 2))
-all_sources = remove_duplicates(all_sources, comb_all)
+# print("Remove duplicates between sources")
+# all_sources = ismdb + daily
 # print(len(all_sources))
-print()
+# comb_all = list(itertools.combinations(all_sources, 2))
+# all_sources = remove_duplicates(all_sources, comb_all)
+# # print(len(all_sources))
+# print()
 
-all_sources += savant
-print(len(all_sources))
-comb_all = list(itertools.combinations(all_sources, 2))
-all_sources = remove_duplicates(all_sources, comb_all)
+# all_sources += savant
 # print(len(all_sources))
-print()
+# comb_all = list(itertools.combinations(all_sources, 2))
+# all_sources = remove_duplicates(all_sources, comb_all)
+# # print(len(all_sources))
+# print()
 
-all_sources += weekly
-print(len(all_sources))
-comb_all = list(itertools.combinations(all_sources, 2))
-all_sources = remove_duplicates(all_sources, comb_all)
+# all_sources += weekly
 # print(len(all_sources))
-print()
+# comb_all = list(itertools.combinations(all_sources, 2))
+# all_sources = remove_duplicates(all_sources, comb_all)
+# # print(len(all_sources))
+# print()
 
-all_sources += screen
-print(len(all_sources))
-comb_all = list(itertools.combinations(all_sources, 2))
-all_sources = remove_duplicates(all_sources, comb_all)
+# all_sources += screen
 # print(len(all_sources))
-print()
+# comb_all = list(itertools.combinations(all_sources, 2))
+# all_sources = remove_duplicates(all_sources, comb_all)
+# # print(len(all_sources))
+# print()
 
-all_sources += awesome
-print(len(all_sources))
-comb_all = list(itertools.combinations(all_sources, 2))
-all_sources = remove_duplicates(all_sources, comb_all)
-print(len(all_sources))
+# all_sources += awesome
+# print(len(all_sources))
+# comb_all = list(itertools.combinations(all_sources, 2))
+# all_sources = remove_duplicates(all_sources, comb_all)
+# print(len(all_sources))
 
-# print(sorted([x.split(sep)[-1] for x in daily if x not in all_sources]))
-# print(sorted([x.split(sep)[-1] for x in all_sources]))
+# # print(sorted([x.split(sep)[-1] for x in daily if x not in all_sources]))
+# # print(sorted([x.split(sep)[-1] for x in all_sources]))
 
-# unfiltered = ismdb + daily + weekly
-
-
-if not exists(DIR_FILTER):
-    makedirs(DIR_FILTER)
-
-
-print("Write cleaned files to new dir")
-for source in tqdm(all_sources):
-    f = open(source, 'r', errors="ignore")
-    data = f.read().strip()
-    f.close()
-
-    with open(join(DIR_FILTER, source.split(sep)[-1]), 'w', errors="ignore") as out:
-        out.write(data)
+# # unfiltered = ismdb + daily + weekly
 
 
-print("Remove different versions of scripts with same name")
-filtered = [join(DIR_FILTER, f) for f in listdir(DIR_FILTER)
-            if isfile(join(DIR_FILTER, f)) and getsize(join(DIR_FILTER, f)) > 3000]
-print(len(filtered))
-comb_filter = list(itertools.combinations(filtered, 2))
-
-for (x, y) in tqdm(comb_filter):
-    result = fuzz.partial_ratio("".join(x.split(sep)[-1].split('.txt')[0].split("-")).lower(),
-                        "".join(y.split(sep)[-1].split('.txt')[0].split("-")).lower())
-    if result > 60:
-        f1 = open(x, 'r', errors="ignore")
-        file_1 = f1.read().replace("\n", " ").replace("\t", " ")[:200]
-        f1.close()
-        f2 = open(y, 'r', errors="ignore")
-        file_2 = f2.read().replace("\n", " ").replace("\t", " ")[:200]
-        f2.close()
-
-        result = fuzz.ratio(file_1, file_2)
-        if result > 80:
-            try:
-                if len(file_2) > len(file_1):
-                    filtered.remove(x)
-                else:
-                    filtered.remove(y)
-            except:
-                pass
-
-print(sorted([x.split(sep)[-1] for x in filtered]))
-print(len(filtered))
-
-if not exists(DIR_FINAL):
-    makedirs(DIR_FINAL)
+# if not exists(DIR_FILTER):
+#     makedirs(DIR_FILTER)
 
 
-print("Write cleaned files to new dir")
-for source in tqdm(filtered):
-    f = open(source, 'r', errors="ignore")
-    data = f.read().strip()
-    f.close()
+# print("Write cleaned files to new dir")
+# for source in tqdm(all_sources):
+#     f = open(source, 'r', errors="ignore")
+#     data = f.read().strip()
+#     f.close()
 
-    with open(join(DIR_FINAL, source.split(sep)[-1]), 'w', errors="ignore") as out:
-        out.write(data)
+#     with open(join(DIR_FILTER, source.split(sep)[-1]), 'w', errors="ignore") as out:
+#         out.write(data)
+
+
+# print("Remove different versions of scripts with same name")
+# filtered = [join(DIR_FILTER, f) for f in listdir(DIR_FILTER)
+#             if isfile(join(DIR_FILTER, f)) and getsize(join(DIR_FILTER, f)) > 3000]
+# print(len(filtered))
+# comb_filter = list(itertools.combinations(filtered, 2))
+
+# for (x, y) in tqdm(comb_filter):
+#     result = fuzz.partial_ratio("".join(x.split(sep)[-1].split('.txt')[0].split("-")).lower(),
+#                         "".join(y.split(sep)[-1].split('.txt')[0].split("-")).lower())
+#     if result > 60:
+#         f1 = open(x, 'r', errors="ignore")
+#         file_1 = f1.read().replace("\n", " ").replace("\t", " ")[:200]
+#         f1.close()
+#         f2 = open(y, 'r', errors="ignore")
+#         file_2 = f2.read().replace("\n", " ").replace("\t", " ")[:200]
+#         f2.close()
+
+#         result = fuzz.ratio(file_1, file_2)
+#         if result > 80:
+#             try:
+#                 if len(file_2) > len(file_1):
+#                     filtered.remove(x)
+#                 else:
+#                     filtered.remove(y)
+#             except:
+#                 pass
+
+# print(sorted([x.split(sep)[-1] for x in filtered]))
+# print(len(filtered))
+
+# if not exists(DIR_FINAL):
+#     makedirs(DIR_FINAL)
+
+
+# print("Write cleaned files to new dir")
+# for source in tqdm(filtered):
+#     f = open(source, 'r', errors="ignore")
+#     data = f.read().strip()
+#     f.close()
+
+#     with open(join(DIR_FINAL, source.split(sep)[-1]), 'w', errors="ignore") as out:
+#         out.write(data)
 
