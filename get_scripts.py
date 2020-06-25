52f4d0df2deb6049ab8300d732742aa0e3807890
Aveek Saha <aveek.s98@gmail.com>
2020-06-25 01:00:00 +0100

    Display source when fetching

--- get_scripts.py
+++ get_scripts.py
@@ -1,8 +1,28 @@
 import sources
 
+print("Fetching from imsdb")
 sources.get_imsdb()
+print()
+
+print("Fetching from screenplays")
 sources.get_screenplays()
+print()
+
+print("Fetching from scriptsavant")
 sources.get_scriptsavant()
+print()
+
+print("Fetching from weeklyscript")
 sources.get_weeklyscript()
+print()
+
+print("Fetching from dailyscript")
 sources.get_dailyscript()
+print()
+
+print("Fetching from awesomefilm")
 sources.get_awesomefilm()
+print()
+
+print("Fetching from sfy")
+sources.get_sfy()
