73adfee1373ca8321d7b35a605b22532527a65d4
Aveek Saha <aveek.s98@gmail.com>
2020-06-25 01:00:00 +0100

    Retry with modified name

--- get_metadata.py
+++ get_metadata.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 from os.path import isfile, join, sep, getsize, exists
 from os import listdir, makedirs
 import re
+from fuzzywuzzy import fuzz
 
 import config
 
@@ -25,14 +26,13 @@ def search_name(name):
     name = " ".join(name.split("-"))
     if re.sub(r"(\d{4})", "", name).strip() != "":
         name = re.sub(r"(\d{4})", "", name)
-    name = " ".join(camel_case_split(name)).lower()
-    name = re.sub(r'\([^)]*\)', '', name).replace('transcript', "").replace(
+    name = " ".join(camel_case_split(re.sub(r'\([^)]*\)', '', name))).lower()
+    name = name.replace('transcript', "").replace(
         'script', "").replace("early draft", "").replace('transcript', "").replace(
             'production', "").replace("final draft", "").replace(
             'unproduced', "").replace("pdf", "").replace(
-            'html', "").replace("doc", "").strip()
-    if name.endswith('the') or name.startswith('the '):
-        name = name.strip('the').strip()
+            'html', "").replace("doc", "").replace(
+            'the ', "").replace(" the", "").strip()
     if name.endswith('final'):
         name = name.replace('final', "").strip()
     if name.endswith('early'):
@@ -55,7 +55,28 @@ for movie in tqdm(movielist):
     if jres['total_results'] > 0:
         mapping[movie.split(sep)[-1].split('.txt')[0]] = jres['results']
     else:
-        mapping[movie.split(sep)[-1].split('.txt')[0]] = []
+        name = movie.split(sep)[-1].split('.txt')[0]
+        response = urllib.request.urlopen(
+            "https://api.themoviedb.org/3/search/movie?api_key=" +
+            tmdb_api_key + "&language=en-US&query=" + urllib.parse.quote(name)
+            + "&page=1")
+        html = response.read()
+        jres = json.loads(html)
+        if jres['total_results'] > 0:
+            mapping[movie.split(sep)[-1].split('.txt')[0]] = jres['results']
+        else:
+            name = " ".join(movie.split(sep)[-1].split('.txt')[0].split("-"))
+            name = " ".join(camel_case_split(name))
+            response = urllib.request.urlopen(
+                "https://api.themoviedb.org/3/search/movie?api_key=" +
+                tmdb_api_key + "&language=en-US&query=" + urllib.parse.quote(name)
+                + "&page=1")
+            html = response.read()
+            jres = json.loads(html)
+            if jres['total_results'] > 0:
+                mapping[movie.split(sep)[-1].split('.txt')[0]] = jres['results']
+            else:
+                mapping[movie.split(sep)[-1].split('.txt')[0]] = []
 
     
     # print(name)
@@ -73,5 +94,22 @@ count = 0
 for key in mapping:
     if len(mapping[key]) == 0:
         count += 1
-        print(search_name(key.split('.txt')[0]))
+        # n = key.split('.txt')[0]
+        # print(search_name(key.split('.txt')[0])," : ", n)
+    elif len(mapping[key]) == 1:
+        name = re.sub(r'\([^)]*\)', '',
+                  " ".join(key.split('.txt')[0].split("-")))
+        m = mapping[key][0]['title'].replace(
+            '\'', '').replace(":", '').replace(",", '').replace(
+            '.', '')
+        if fuzz.token_sort_ratio(name,  m) < 60:
+            print(key.split('.txt')[0], " : ", mapping[key][0]['title'])
+
+    # else:
+    #     name = re.sub(r'\([^)]*\)', '',
+    #                   " ".join(key.split('.txt')[0].split("-")))
+    #     print(key.split('.txt')[0], " : ", [
+    #           (x['title'], fuzz.token_sort_ratio(name, 
+    #           x['title'].replace('\'', '').replace(":", '').replace(
+    #               ",", ''))) for x in mapping[key][:2]])
 print(count)
