d6aac5f18db91bd4a9672aef566ffa9818368264
Aveek Saha <aveek.s98@gmail.com>
2021-07-02 21:26:46 +0530

    Simplify main script

--- get_scripts.py
+++ get_scripts.py
@@ -6,24 +6,9 @@ f = open('sources.json','r')
 data = json.load(f)
 
 for source in data:
-    print("Fetching scripts from %s" % (source))
     included = data[source]
-    if source == "imsdb" and included == "true":
-        sources.get_imsdb()
-    elif source == "screenplays" and included == "true":
-        sources.get_screenplays()
-    elif source == "scriptsavant" and included == "true":
-        sources.get_scriptsavant()
-    elif source == "weeklyscript" and included == "true":
-        sources.get_weeklyscript()
-    elif source == "dailyscript" and included == "true":
-        sources.get_dailyscript()
-    elif source == "awesomefilm" and included == "true":
-        sources.get_awesomefilm()
-    elif source == "sfy" and included == "true":
-        sources.get_sfy()
-    elif source == "scriptslug" and included == "true":
-        sources.get_scriptslug()
-    
-    print()
+    if included == "true":
+        print("Fetching scripts from %s" % (source))
+        sources.get_scripts(source=source)
+        print()
 
--- sources/__init__.py
+++ sources/__init__.py
@@ -7,3 +7,23 @@ from .awesomefilm import get_awesomefilm
 from .sfy import get_sfy
 from .scriptslug import get_scriptslug
 from .utilities import *
+
+def get_scripts(source):
+    if source == "imsdb":
+        get_imsdb()
+    elif source == "screenplays":
+        get_screenplays()
+    elif source == "scriptsavant":
+        get_scriptsavant()
+    elif source == "weeklyscript":
+        get_weeklyscript()
+    elif source == "dailyscript":
+        get_dailyscript()
+    elif source == "awesomefilm":
+        get_awesomefilm()
+    elif source == "sfy":
+        get_sfy()
+    elif source == "scriptslug":
+        get_scriptslug()
+    else:
+        print("Invalid source.")
