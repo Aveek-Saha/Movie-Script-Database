d1515d198f3776a939a17729d8ac0d318f51b4ce
Aveek Saha <aveek.s98@gmail.com>
2021-07-10 12:37:16 +0530

    Create metadata file for scriptslug

--- sources/scriptslug.py
+++ sources/scriptslug.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import urllib
 import os
 import re
+import json
 
 from tqdm import tqdm
 from .utilities import format_filename, get_soup, get_pdf_text
@@ -12,16 +13,18 @@ def get_scriptslug():
     ALL_URL = "https://www.scriptslug.com/request/?pg="
     BASE_URL = "https://www.scriptslug.com/assets/uploads/scripts/"
     DIR = os.path.join("scripts", "unprocessed", "scriptslug")
+    META_DIR = os.path.join("scripts", "metadata")
 
     if not os.path.exists(DIR):
         os.makedirs(DIR)
+    if not os.path.exists(META_DIR):
+        os.makedirs(META_DIR)
 
     def get_script_from_url(script_url):
         text = ""
 
         try:
-            text = get_pdf_text(
-                BASE_URL + urllib.parse.quote(script_url) + ".pdf")
+            text = get_pdf_text(script_url)
             return text
 
         except Exception as err:
@@ -35,11 +38,12 @@ def get_scriptslug():
         script_url = movie['href'].split("/")[-1]
 
         name = movie.find_all(class_="script__title")[
-            0].find(text=True, recursive=False)
-        file_name = re.sub(r'\([^)]*\)', '', format_filename(name.strip()))
+            0].find(text=True, recursive=False).strip()
+        file_name = re.sub(r'\([^)]*\)', '', format_filename(name))
 
-        return script_url, file_name
+        return script_url, file_name, name
 
+    metadata = {}
     movielist = []
 
     for num in range(25):
@@ -49,10 +53,19 @@ def get_scriptslug():
         movielist.extend(linklist)
 
     for movie in tqdm(movielist):
-        script_url, name = get_script_url(movie)
+        script_url, file_name, name = get_script_url(movie)
+        script_url = BASE_URL + urllib.parse.quote(script_url) + ".pdf"
         text = get_script_from_url(script_url)
         if text == "" or name == "":
             continue
 
-        with open(os.path.join(DIR, name + '.txt'), 'w', errors="ignore") as out:
+        metadata[name] = {
+            "file_name": file_name,
+            "script_url": script_url
+        }
+
+        with open(os.path.join(DIR, file_name + '.txt'), 'w', errors="ignore") as out:
             out.write(text)
+    
+    with open(os.path.join(META_DIR, "scriptslug.json"), "w") as outfile: 
+        json.dump(metadata, outfile, indent=4)
