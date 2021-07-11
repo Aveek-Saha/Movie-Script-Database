8ac9bf0c460e8566c55771689cfe7d5862a4b75e
Aveek Saha <aveek.s98@gmail.com>
2021-07-11 12:51:44 +0530

    Create metadata file for actorpoint

--- sources/actorpoint.py
+++ sources/actorpoint.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import urllib
 import os
+import json
 from tqdm import tqdm
 import string
 import re
@@ -11,17 +12,21 @@ def get_actorpoint():
     ALL_URL = "https://www.actorpoint.com/movie-scripts/mscr-%s.html"
     BASE_URL = "https://www.actorpoint.com"
     DIR = os.path.join("scripts", "unprocessed", "actorpoint")
+    META_DIR = os.path.join("scripts", "metadata")
 
     if not os.path.exists(DIR):
         os.makedirs(DIR)
+    if not os.path.exists(META_DIR):
+        os.makedirs(META_DIR)
 
     def get_script_from_url(script_url):
         text = ""
 
         try:
             if script_url.endswith('.html'):
-                script_soup = get_soup(
-                    BASE_URL + urllib.parse.quote(script_url))
+                script_soup = get_soup(script_url)
+                if script_soup == None:
+                    return ""
                 text = script_soup.pre.get_text()
             else:
                 print("No script at " + script_url)
@@ -36,14 +41,15 @@ def get_actorpoint():
 
         script_url = movie.a['href']
 
-        name = movie.a.text
-        file_name = re.sub(r'\([^)]*\)', '', format_filename(name.strip()))
+        name = re.sub(r'\([^)]*\)', '', movie.a.text.strip())
+        file_name = format_filename(name)
 
-        return script_url, file_name
+        return script_url, file_name, name
 
     alphabet = string.ascii_lowercase
-
+    metadata = {}
     movielist = []
+
     for letter in alphabet:
         soup = get_soup(ALL_URL % (letter))
         movielist.extend(soup.find_all(attrs={"data-th": "Script name"}))
@@ -51,10 +57,20 @@ def get_actorpoint():
     movielist.extend(soup.find_all(attrs={"data-th": "Script name"}))
 
     for movie in tqdm(movielist):
-        script_url, name = get_script_url(movie)
+        script_url, file_name, name = get_script_url(movie)
+        script_url = BASE_URL + urllib.parse.quote(script_url)
+
         text = get_script_from_url(script_url)
         if text == "" or name == "":
             continue
 
-        with open(os.path.join(DIR, name + '.txt'), 'w', errors="ignore") as out:
+        metadata[name] = {
+            "file_name": file_name,
+            "script_url": script_url
+        }
+
+        with open(os.path.join(DIR, file_name + '.txt'), 'w', errors="ignore") as out:
             out.write(text)
+    
+    with open(os.path.join(META_DIR, "actorpoint.json"), "w") as outfile: 
+        json.dump(metadata, outfile, indent=4)
