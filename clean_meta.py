2361d78a86ff7b75489ae17e3bd004ce53d7286a
Aveek Saha <aveek.s98@gmail.com>
2021-07-17 08:56:26 +0530

    Store duplicate data

--- clean_meta.py
+++ clean_meta.py
@@ -24,6 +24,10 @@ forbidden = ["the", "a", "an", "and", "or", "part",
 unique = []
 origin = {}
 for source in metadata:
+    DIR = join("scripts", "unprocessed", source)
+    files = [join(DIR, f) for f in listdir(DIR) if isfile(
+        join(DIR, f)) and getsize(join(DIR, f)) > 3000]
+    
     source_meta = metadata[source]
     for script in source_meta:
         name = re.sub(r'\([^)]*\)', '', script.strip()).lower()
@@ -36,11 +40,28 @@ for source in metadata:
         unique.append(name)
         if name not in origin:
             origin[name] = []
-        origin[name].append(source)
+        curr_script = metadata[source][script]
+        curr_file = join("scripts", "unprocessed", source, curr_script["file_name"] + ".txt") 
+        
+        if curr_file in files:
+            file_info = {
+                source: {
+                        "name": script,
+                        "file_name": curr_script["file_name"],
+                        "script_url": curr_script["script_url"],
+                        "size": getsize(curr_file)
+                    }
+                }
+            origin[name].append(file_info)
 
 final = sorted(list(set(unique)))
-print(final)
-print(len(final))
+# print(final)
+# print(len(final))
+
+with open(join(META_DIR, "clean_meta.json"), "w") as outfile:
+    json.dump(origin, outfile, indent=4)
+
+print(len(origin))
 
 # for script in final:
 #     print(origin[script])
