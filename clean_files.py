0f9ca3f6e9e0132f22a99aa0c29e51d9ac04d0d9
Aveek Saha <aveek.s98@gmail.com>
2021-07-21 19:51:48 +0530

    Identify identical files

--- clean_files.py
+++ clean_files.py
@@ -56,22 +56,72 @@ def clean_script(text):
     final_data = '\n'.join(lines)
     return final_data
 
+def compare_scripts(scripts):
+    combos = list(itertools.combinations(scripts, 2))
+    count_same = 0
 
+    for combo in combos:
+        if combo[0]["text"] == combo[1]["text"]:
+            # print(combo[0]["file_name"], combo[1]["file_name"])
+            count_same += 1
+    if len(combos) == count_same:
+        # print(scripts[0]["file_name"])
+        return 1
+    else:
+        return 0
+
+
+count = 0
+count_total = 0
 for script in tqdm(metadata):
     files = metadata[script]["files"]
-    if len(files) == 1:
-        file = join(SCRIPT_DIR, files[0]["source"],
-                    files[0]["file_name"] + ".txt")
-        f = open(file, 'r', errors="ignore")
-        text = f.read()
-        f.close()
-        final_data = clean_script(text)
+    # if len(files) == 1:
+    #     file = join(SCRIPT_DIR, files[0]["source"],
+    #                 files[0]["file_name"] + ".txt")
+    #     f = open(file, 'r', errors="ignore")
+    #     text = f.read()
+    #     f.close()
+    #     final_data = clean_script(text)
 
-        if final_data.strip() == "":
-            print(files)
+    #     if final_data.strip() == "":
+    #         print(files)
 
-        with open(join(CLEAN_DIR, files[0]["source"] + "_" + files[0]["file_name"] + ".txt"), 'w', errors="ignore") as out:
-            out.write(final_data)
+    #     with open(join(CLEAN_DIR, files[0]["source"] + "_" + files[0]["file_name"] + ".txt"), 'w', errors="ignore") as out:
+    #         out.write(final_data)
 
-    if  len(files) == 4:
-        print(files)
+    # if  len(files) == 2:
+        
+    #     file_1 = join(SCRIPT_DIR, files[0]["source"],
+    #                 files[0]["file_name"] + ".txt")
+    #     file_2 = join(SCRIPT_DIR, files[1]["source"],
+    #                 files[1]["file_name"] + ".txt")
+    #     f_1 = open(file_1, 'r', errors="ignore")
+    #     f_2 = open(file_2, 'r', errors="ignore")
+    #     text_1 = f_1.read()
+    #     text_2 = f_2.read()
+    #     f_1.close()
+    #     f_2.close()
+    #     final_data_1 = clean_script(text_1)
+    #     final_data_2 = clean_script(text_2)
+    #     if final_data_1[:10000] == final_data_2[:10000]:
+    #         count +=1
+    #         # print(files)
+    #     count_total += 1
+
+    if  len(files) != 1:
+        script_arr = []
+
+        for file in files:
+            path = join(SCRIPT_DIR, file["source"],
+                    file["file_name"] + ".txt")
+            f = open(path, 'r', errors="ignore")
+            text = f.read()
+            f.close()
+            clean_text = clean_script(text)
+            file["text"] = clean_text[:10000]
+
+            script_arr.append(file)
+        count += compare_scripts(script_arr)
+
+print(count)
+# print(count_total)
