a9f500850b75f43fe9d8d21f8f38b7649fd6d916
copoer <c@ooper.space>
2021-09-18 02:33:33 +0100

    Added fixes

--- get_occurances.py
+++ get_occurances.py
@@ -9,7 +9,7 @@ if not os.path.exists(WRITE_DIR):
     os.makedirs(WRITE_DIR)
 
 for filename in tqdm(glob.glob('scripts/parsed/dialogue/*.txt')):
-   with open(os.path.join(os.getcwd(), filename), 'r') as f:
+   with open(os.path.join(os.getcwd(), filename), 'r', encoding='utf-8') as f:
        words = dict()
        for line in f:
            dialogue = line.split(">",1)[1].lower()
@@ -22,7 +22,8 @@ for filename in tqdm(glob.glob('scripts/parsed/dialogue/*.txt')):
                        words[word] += 1
                    else:
                        words[word] = 1
-       words = dict(sorted(words.items(), key=lambda item: item[1]))
+       sorted_words = dict(sorted(words.items(), key=lambda item: item[1]))
        clean_filename = Path(filename).stem
+       clean_filename = clean_filename.replace('_dialogue', '')
        with open(WRITE_DIR+clean_filename+'.json', 'w') as json_file:
-           json.dump(words, json_file, indent=4,  sort_keys=True, separators=(',',':'))
+           json.dump(sorted_words, json_file, indent=4, separators=(',',':'))
